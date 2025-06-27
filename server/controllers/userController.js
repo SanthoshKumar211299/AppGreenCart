3a852d23495d02f5d900eeb4638eb0f71d9081f3
SanthoshKumar211299 <subjansan@gmail.com>
2025-06-27 14:22:22 +0530

    userId name changed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,7 +18,7 @@ export const register = async (req,res)=> {
 
             const user = await User.create ({name,email,password:hashedPassword})
 
-            const token =jwt.sign({id:User._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+            const token =jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
 
             res.cookie('token' , token, {
                 httpOnly:true, //prevent javascript to access cookie
